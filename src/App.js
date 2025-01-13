fd5ca9fbef25b8b6aec350b3232c1c155a16da16
Alaafathalla <alaafathalla36@gmail.com>
2025-01-14 00:41:49 +0200

    home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
-
 import './App.css';
 import Navbar from './components/NavBar/Navbar';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
 <Navbar/>
-
+<Home/>
     </div>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React from 'react';
+import home from '../../assests/layer-slider-model.png';
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div className="container-fluid home">
+        <div className="row align-items-center">
+          {/* Left Section */}
+          <div className="col-md-6 p-5">
+            <h1>
+              Huge collection <br />
+              of <span className="text-primary">Best Books</span>
+            </h1>
+            <p>
+              An online book is a resource in book-like form that is only available to read on the Internet. It differs from the common idea of an e-book.
+            </p>
+            <div className="input-group mb-3">
+              <label htmlFor="searchInput" className="visually-hidden">
+                Search for books
+              </label>
+              <input
+                id="searchInput"
+                type="text"
+                className="form-control"
+                placeholder="Search for something..."
+                aria-label="Search input"
+                aria-describedby="button-addon2"
+              />
+              <button
+                className="btn btn-outline-primary"
+                type="button"
+                id="button-addon2"
+              >
+                Search
+              </button>
+            </div>
+          </div>
+          
+          {/* Right Section */}
+          <div className="col-md-6 text-center">
+            <img
+              src={home}
+              alt="A large collection of books"
+              className="img-fluid"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
