f62e6150e4650e55199343bf902b3163b7843426
Alaafathalla <alaafathalla36@gmail.com>
2025-01-13 03:27:47 +0200

    nav

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,30 +1,14 @@
 import React from 'react';
-
+// import logo from '../assets/logo-book-shop.png';
 export default function Navbar() {
   return (
     <div className="container-fluid ">
 <nav className="navbar navbar-expand-lg">
   <div className="container">
-    <div className="row w-100">
-      <div className="col-6">
         <a className="navbar-brand" href="#">
-          Navbar
+      {/* <img src={logo} alt="" srcset="" /> */}
+      LOGO
         </a>
-      </div>
-      <div className="col-6 text-end">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-      </div>
-    </div>
 
     {/* Second Row: Links and Buttons */}
     <div className="row w-100">
