3742c66d3407041544dbfed7df56d6bbde1e04a0
Alaafathalla <alaafathalla36@gmail.com>
2025-03-12 02:05:15 +0200

    form edits

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaEnvelope } from 'react-icons/fa';
+import './Footer.css'
 import logo from '../../assests/logo.png';
 
 export default function Footer() {
@@ -36,24 +37,22 @@ export default function Footer() {
       {/* Footer Content Section */}
       <div className="container p-5 rounded-5">
         <div className="row">
-          {/* Column 1: Logo & Description */}
           <div className="col-md-3">
             <img src={logo} alt="Logo" style={{ width: "150px" }} />
             <p className="mt-3">Discover the best books across different genres. Join our community today.</p>
           </div>
 
-          {/* Column 2: Quick Links */}
           <div className="col-md-3">
             <h5 className="fw-bold">Quick Links</h5>
             <ul className="list-unstyled">
-              <li><Link to="/" className="text-white text-decoration-none">Home</Link></li>
+              <li><Link to="/" className="text-white  text-decoration-none">Home</Link></li>
               <li><Link to="/books" className="text-white text-decoration-none">Books</Link></li>
               <li><Link to="/about" className="text-white text-decoration-none">About Us</Link></li>
               <li><Link to="/contact" className="text-white text-decoration-none">Contact</Link></li>
             </ul>
           </div>
 
-          {/* Column 3: Categories */}
+
           <div className="col-md-3">
             <h5 className="fw-bold">Categories</h5>
             <ul className="list-unstyled">
